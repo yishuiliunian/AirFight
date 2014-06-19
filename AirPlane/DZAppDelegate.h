9b2ee0873b75e8504dff5bf49babb013ea019ab0
stonedong <yishuiliunian@gmail.com>
2014-06-19 20:10:34 +0800

    Initial Commit

---
+++ AirPlane/DZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DZAppDelegate.h
+//  AirPlane
+//
+//  Created by stonedong on 14-6-19.
+//  Copyright (c) 2014年 stonedong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AirPlane/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AirPlane
+//
+//  Created by stonedong on 14-6-19.
+//  Copyright (c) 2014年 stonedong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DZAppDelegate class]));
+    }
+}
